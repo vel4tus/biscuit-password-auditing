c76ae5c77c517ab7f4c0dd535449e7e6a53b414d
vel4tus <vel4tus@proton.me>
2026-08-26 21:49:50 +0300

    add custom argument validation for hash-gen mode

--- src/biscuit/modes/hashgen.py
+++ src/biscuit/modes/hashgen.py
@@ -3,5 +3,24 @@
 from biscuit.hashing import compute_hash
 
 
+# Valdates given arguments, searches for the missing ones before executing the attack
+def validate_args(password: str, algorithm: str) -> bool:
+    missing = []
+
+    if not password:
+        missing.append("--hash")
+    if not algorithm:
+        missing.append("--algorithm")
+
+    if missing:
+        print(f"error: the following arguments are required: {", ".join(missing)}")
+        return False
+    else:
+        return True
+
+
 def main(password: str, algorithm: str) -> None:
+    if not validate_args(password, algorithm):
+        return
+
     print(compute_hash(password, algorithm).hex())
